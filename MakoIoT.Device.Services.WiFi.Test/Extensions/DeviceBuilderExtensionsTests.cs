04bc841095c099e6f0347066810585cc8baa8bb0
Marcin Torba <torbamarcin@gmail.com>
2023-01-31 01:17:29 +0100

    Move to nano DI + tests.

---
+++ MakoIoT.Device.Services.WiFi.Test/Extensions/DeviceBuilderExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using MakoIoT.Device.Services.Interface;
+using MakoIoT.Device.Services.WiFi.Extensions;
+using MakoIoT.Device.Services.WiFi.Test.Mocks;
+using nanoFramework.DependencyInjection;
+using nanoFramework.TestFramework;
+
+namespace MakoIoT.Device.Services.WiFi.Test.Extensions
+{
+    [TestClass]
+    public class DeviceBuilderExtensionsTests
+    {
+        [TestMethod]
+        public void AddWiFi_Should_RegisterServicesAndReturnDevice()
+        {
+            var deviceBuilder = new DeviceBuilderMock();
+            
+            DeviceBuilderExtension.AddWiFi(deviceBuilder);
+            var serviceProvider = deviceBuilder.Services.BuildServiceProvider();
+
+            var service = serviceProvider.GetService(typeof(INetworkProvider));
+            Assert.IsNotNull(service);
+        }
+    }
+}
