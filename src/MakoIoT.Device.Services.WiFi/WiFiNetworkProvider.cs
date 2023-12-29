4fb52346104d48ae482217503363e596155a7a32
Marcin Torba <torbamarcin@gmail.com>
2023-12-29 19:19:36 +0100

    Remove WiFiInterfaceManager and refactor WiFiNetworkProvider (#30)
    Removed the WiFiInterfaceManager file as it was no longer needed in the project. In WiFiNetworkProvider, refactored the Connect function to improve clarity and functionality, shifting the configuration fetching moment to a more suitable point in the execution flow.

--- src/MakoIoT.Device.Services.WiFi/WiFiNetworkProvider.cs
+++ src/MakoIoT.Device.Services.WiFi/WiFiNetworkProvider.cs
@@ -8,12 +8,12 @@ namespace MakoIoT.Device.Services.WiFi
 {
     public class WiFiNetworkProvider : INetworkProvider
     {
-        private readonly WiFiConfig _wifiConfig;
+        private readonly IConfigurationService _configService;
         private readonly ILog _logger;
 
         public WiFiNetworkProvider(IConfigurationService configService, ILog logger)
         {
-            _wifiConfig = (WiFiConfig)configService.GetConfigSection(WiFiConfig.SectionName, typeof(WiFiConfig));
+            _configService = configService;
             _logger = logger;
         }
 
@@ -23,18 +23,21 @@ namespace MakoIoT.Device.Services.WiFi
 
         public void Connect()
         {
-            if (!IsConnected)
+            if (IsConnected)
             {
-                _logger.Trace($"Network status: {WifiNetworkHelper.Status} - connecting");
-                CancellationTokenSource cs = new(_wifiConfig.ConnectionTimeout * 1000);
-                bool success = WifiNetworkHelper.ConnectDhcp(_wifiConfig.Ssid, _wifiConfig.Password, requiresDateTime: true, token: cs.Token);
-                _logger.Trace($"WifiNetworkHelper.ConnectDhcp result: {success}");
-                if (!success)
-                {
-                    _logger.Error( "Network connection error", WifiNetworkHelper.HelperException);
-                }
+                _logger.Trace($"Network status: {WifiNetworkHelper.Status}");
+                return;
             }
-            _logger.Trace($"Network status: {WifiNetworkHelper.Status}");
+            
+            _logger.Trace($"Network status: {WifiNetworkHelper.Status} - connecting");
+            var configSection = (WiFiConfig)_configService.GetConfigSection(WiFiConfig.SectionName, typeof(WiFiConfig));
+            var cs = new CancellationTokenSource(configSection.ConnectionTimeout * 1000);
+            var success = WifiNetworkHelper.ConnectDhcp(configSection.Ssid, configSection.Password, requiresDateTime: true, token: cs.Token);
+            _logger.Trace($"WifiNetworkHelper.ConnectDhcp result: {success}");
+            if (!success)
+            {
+                _logger.Error( "Network connection error", WifiNetworkHelper.HelperException);
+            }
         }
 
         public void Disconnect()
